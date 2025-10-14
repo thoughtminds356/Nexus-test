d5507234e21c0f13abce3bed7194122837ee88b4
Vishnu Dinesh <vishnu.dinesh@thoughtminds.io>
2025-10-14 12:47:37 +0530

    serve media - undo

--- backend/myproject/urls.py
+++ backend/myproject/urls.py
@@ -15,18 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from myapp.views.media import serve_media
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('myapp.urls')),  # Include URLs from myapp
-    # Custom media serving with CORS headers
-    re_path(r'^media/(?P<path>.*)$', serve_media, name='serve_media'),
 ]
 
-# Only serve static files in DEBUG mode through Django
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
